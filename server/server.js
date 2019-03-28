b06dcc6687f9fab1ae63f55ff1ebc7168d033510
jakub-g <jakub.g.opensource@gmail.com>
2019-03-28 16:30:33 +0100

    yellow-lime

--- server/server.js
+++ server/server.js
@@ -9,20 +9,25 @@ const app = express()
 app.get('/', function resetHandler(req, res) {
     res.write(`<!doctype html>
       <body bgcolor=yellow>
-      <h1>Streaming HTML parser demo (looks like it's Chrome only)...</h1>
-      <p>responseStart</p>
-      <script>window._scriptStartTime = performance.now()</script>`)
+      <h1>Streaming HTML parser demo (looks like it's Chrome only)</h1>
+      <p>If this page loads first yellow, then turns lime, streaming parser is working really well.<br>
+         If the first paint you see is lime, it means HTML is only rendered on screen after responseEnd.</p>
+      <script>window._scriptStartTime = performance.now()</script>
+      <p>responseStart...
+    `)
 
     setTimeout(function() {
-      res.write(`<p>responseEnd</p>
+      res.write(`responseEnd</p>
+
         <script>
+        document.body.style.backgroundColor = 'lime'
         setTimeout(() => {
           let msg = ''
           msg += '\\nresponseStart = ' + Math.round(performance.getEntriesByType('navigation')[0].responseStart)
           msg += '\\nscriptStart   = ' + Math.round(window._scriptStartTime)
           msg += '\\nresponseEnd    = ' + Math.round(performance.getEntriesByType('navigation')[0].responseEnd)
           alert(msg)
-        }, 1000)
+        }, 0)
         </script>`)
       res.end()
     }, 3000)
